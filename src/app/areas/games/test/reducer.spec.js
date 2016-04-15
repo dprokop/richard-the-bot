85fdd8668d555d670efcd86359dc7f8804c88120
Dominik Prokop <prokopd@gmail.com>
2016-04-15 15:26:31 +0200

    Added games reducer tests

--- src/app/areas/games/test/reducer.spec.js
+++ src/app/areas/games/test/reducer.spec.js
@@ -1,51 +1,184 @@
 /* global describe, beforeEach, it, expect*/
 
-import deepFreeze from 'deep-freeze'
+var deepFreeze = require('deep-freeze')
+var games = require('../reducer.js').games
+var assert = require('assert')
 
-import { games } from '../reducer'
-
-describe('Games reducer', function () {
-  beforeEach(function () {
-
+describe('Games reducer', () => {
+  var timestamp
+  beforeEach(() => {
+    timestamp = Date.now()
   })
 
-  it('creates new game onject', function () {
-  /**
-   * @todo define state before and state after for comparison
-   */
-    var stateBefore = []
-    var stateAfter = {}
-    var timestamp = Date.now()
-
-    stateAfter[timestamp] = Object.create({
-      meta: {
-        chanel: 1,
-        organizer: 'Dominik',
-        createdAt: timestamp
-      },
-      players: {
-        available: [],
-        pending: [],
-        accepted: [],
-        rejected: []
+  it('adds new game object', () => {
+    var stateBefore = {}
+    var stateAfter = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp
+        },
+        players: {
+          available: [],
+          pending: [],
+          accepted: [],
+          rejected: []
+        }
       }
-    })
-    deepFreeze(stateBefore)
+    }
 
-        /**
-         * @todo Update payload to fit your model
-         */
     var action = {
       type: 'ADD_GAME',
       payload: {
+        id: 'abc',
         channel: 1,
         organizer: 'Dominik',
         createdAt: timestamp
       }
     }
-    var result = games(stateBefore, action)
-    console.log(stateAfter)
 
-    // expect(result).toEqual(stateAfter)
+    deepFreeze(stateBefore)
+    var result = games(stateBefore, action)
+    assert.deepEqual(result, stateAfter)
+  })
+
+  it('sets start time timestamp for a game that is started', () => {
+    var stateBefore = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp
+        },
+        players: {
+          available: [],
+          pending: [],
+          accepted: [],
+          rejected: []
+        }
+      }
+    }
+
+    var stateAfter = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp,
+          startedAt: timestamp
+        },
+        players: {
+          available: [],
+          pending: [],
+          accepted: [],
+          rejected: []
+        }
+      }
+    }
+
+    var action = {
+      type: 'START_GAME',
+      payload: {
+        gameId: 'abc',
+        startedAt: timestamp
+      }
+    }
+
+    var result = games(stateBefore, action)
+    assert.deepEqual(result, stateAfter)
+  })
+
+  it('updates players arrays for a given game', () => {
+    var stateBefore = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp
+        },
+        players: {
+          available: [],
+          pending: [],
+          accepted: [],
+          rejected: []
+        }
+      }
+    }
+
+    var stateAfter = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp
+        },
+        players: {
+          available: [1,2,3],
+          pending: [4,5,6],
+          accepted: [],
+          rejected: []
+        }
+      }
+    }
+
+    var action = {
+      type: 'UPDATE_PLAYERS',
+      payload: {
+        gameId: 'abc',
+        players: {
+          available: [1,2,3],
+          pending: [4,5,6],
+        }
+      }
+    }
+
+    var result = games(stateBefore, action)
+    assert.deepEqual(result, stateAfter)
+  })
+
+  it('sets pending players to the ones that are drawn', () => {
+    var action = {
+      type: 'RANDOMIZE_PLAYERS',
+      payload: {
+        gameId: 'abc',
+        playerIds: [0,5,2]
+      }
+    }
+
+    var stateBefore = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp
+        },
+        players: {
+          available: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          pending: [],
+          accepted: [],
+          rejected: []
+        }
+      }
+    }
+
+    var stateAfter = {
+      'abc': {
+        meta: {
+          channel: 1,
+          organizer: 'Dominik',
+          createdAt: timestamp
+        },
+        players: {
+          available: [2, 4, 5, 7, 8, 9, 10],
+          pending: [1, 6, 3],
+          accepted: [],
+          rejected: []
+        }
+      }
+    }
+
+    var result = games(stateBefore, action)
+    assert.deepEqual(result, stateAfter)
   })
 })
